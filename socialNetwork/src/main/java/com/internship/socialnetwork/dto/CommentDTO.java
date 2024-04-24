c95850344323a8cd245026089084f47dae86fa0c
Bojana Popov <96697478+popovbojana@users.noreply.github.com>
2024-04-24 14:02:30 +0200

    Refactor comment dto (#24)

--- socialNetwork/src/main/java/com/internship/socialnetwork/dto/CommentDTO.java
+++ socialNetwork/src/main/java/com/internship/socialnetwork/dto/CommentDTO.java
@@ -6,7 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,25 +15,31 @@ import java.util.Optional;
 @AllArgsConstructor
 public class CommentDTO {
 
+    private Long id;
+
     private Long postId;
 
     private Long userId;
 
     private String comment;
 
-    private LocalDateTime commentedAt;
+    private String commentedAt;
 
     private List<CommentDTO> replies;
 
+    private Long parentCommentId;
+
     public static CommentDTO toCommentDTO(Comment comment) {
         return CommentDTO.builder()
+                .id(comment.getId())
                 .postId(comment.getPost().getId())
                 .userId(comment.getCommentedBy().getId())
                 .comment(comment.getComment())
-                .commentedAt(comment.getCommentedAt())
+                .commentedAt(comment.getCommentedAt().toString())
                 .replies(Optional.ofNullable(comment.getReplies())
                         .map(replies -> replies.stream().map(CommentDTO::toCommentDTO).toList())
                         .orElse(null))
+                .parentCommentId(comment.getParentComment() != null ? comment.getParentComment().getId() : null)
                 .build();
     }
 
